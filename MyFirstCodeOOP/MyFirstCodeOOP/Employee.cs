c6ef9d3677ad3c35bbf2fbac4eb975131d32e312
cesar barrientos <ceaubacor98@hotmail.com>
2023-02-15 18:59:21 -0500

    Create SalaryEmployee, Employee class

---
+++ MyFirstCodeOOP/MyFirstCodeOOP/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstCodeOOP
+{
+    public abstract class Employee
+    {
+        #region Properties
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Date BirthDate { get; set; }
+        public Date HiringDate { get; set; }
+        public bool IsActive { get; set; }
+
+        
+        #endregion
+        #region Methods
+        public abstract decimal GetValueTopay();
+
+        public override string ToString()
+        {
+            return $"--EMPLOYEE--\n\t ID:{Id} \n\t First Name: {FirstName}" +
+                $"\n\t Last Name: {LastName} \n\t Birth Day:{BirthDate}" +
+                $"\n\t Hiring Date:{HiringDate} \n\t Is Active?:{IsActive}";
+        }
+
+        #endregion
+    }
+}
---
+++ MyFirstCodeOOP/MyFirstCodeOOP/SalaryEmployee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstCodeOOP
+{
+    public class SalaryEmployee : Employee
+    {
+        #region Properties
+            public decimal Salary { get; set; }
+        #endregion
+
+        #region Methods
+        public override decimal GetValueTopay()
+        {
+           return Salary;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()} \n\t Value to pay: {GetValueTopay()}";
+        }
+
+        #endregion
+    }
+}
