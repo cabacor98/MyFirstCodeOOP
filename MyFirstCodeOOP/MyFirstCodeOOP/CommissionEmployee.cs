55dbed5c9672a01d07a292a3a4218e36b11d8251
cesar barrientos <ceaubacor98@hotmail.com>
2023-02-20 19:08:19 -0500

    Create a new class CommissionEmloyee

---
+++ MyFirstCodeOOP/MyFirstCodeOOP/CommissionEmployee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstCodeOOP
+{
+    public class CommissionEmployee : Employee
+    {
+        #region Properties
+        public float CommissionPercentage { get; set; }
+        public decimal Sales { get; set; } 
+        #endregion
+
+        #region Methods
+        public override decimal GetValueTopay()
+        {
+            return Sales * (decimal)CommissionPercentage / 100;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}\n\t Comission Percentage: {CommissionPercentage:P2} \n\t Sales: {Sales:C2} \n\t Value to pay: {GetValueTopay():C2}";
+        }
+        #endregion
+
+    }
+}
--- MyFirstCodeOOP/MyFirstCodeOOP/Program.cs
+++ MyFirstCodeOOP/MyFirstCodeOOP/Program.cs
@@ -11,31 +11,47 @@ namespace MyFirstCodeOOP
             int day;
             try
             {
-                Console.WriteLine("Enter the Year:");
-                year = int.Parse(Console.ReadLine());
+                //Console.WriteLine("Enter the Year:");
+                //year = int.Parse(Console.ReadLine());
 
-                Console.WriteLine("Enter the Month:");
-                month = int.Parse(Console.ReadLine());
+                //Console.WriteLine("Enter the Month:");
+                //month = int.Parse(Console.ReadLine());
 
-                Console.WriteLine("Enter the Day:");
-                day = int.Parse(Console.ReadLine());
+                //Console.WriteLine("Enter the Day:");
+                //day = int.Parse(Console.ReadLine());
 
-                var dateObject = new Date(year, month, day);
-                Console.WriteLine(dateObject.ToString());
+                //var dateObject = new Date(year, month, day);
+                //Console.WriteLine(dateObject.ToString());
 
-                Console.WriteLine("-------------------New class-----------------");
+                //Console.WriteLine("-------------------asalary class-----------------");
+                
 
-                Employee salaryEmployee = new SalaryEmployee()
+                //Employee salaryEmployee = new SalaryEmployee()
+                //{
+                //    Id= int.Parse(Console.ReadLine()),
+                //    FirstName = "maria",
+                //    LastName= "Posada",
+                //    BirthDate=new Date(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())),
+                //    HiringDate = new Date(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())),
+                //    IsActive=true,
+                //    Salary = decimal.Parse(Console.ReadLine())
+                //};
+                //Console.WriteLine(salaryEmployee);
+
+                Console.WriteLine("-------------------commission class-----------------");
+
+                Employee CommissionEmployee = new CommissionEmployee()
                 {
-                    Id= int.Parse(Console.ReadLine()),
-                    FirstName = "Maria",
-                    LastName= "Posada",
-                    BirthDate=new Date(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())),
+                    Id = int.Parse(Console.ReadLine()),
+                    FirstName = "maria",
+                    LastName = "Posada",
+                    BirthDate = new Date(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())),
                     HiringDate = new Date(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())),
-                    IsActive=true,
-                    Salary = decimal.Parse(Console.ReadLine())
+                    IsActive = true,
+                    Sales = decimal.Parse(Console.ReadLine()),
+                    CommissionPercentage = float.Parse(Console.ReadLine()),
                 };
-                Console.WriteLine(salaryEmployee);
+                Console.WriteLine(CommissionEmployee);
             }
             catch (Exception ex)
             {
