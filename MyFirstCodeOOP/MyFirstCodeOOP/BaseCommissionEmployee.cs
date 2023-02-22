10a06f6043e4d50491071afd2f62b58a56e78173
cesar barrientos <ceaubacor98@hotmail.com>
2023-02-22 18:05:19 -0500

    update code

---
+++ MyFirstCodeOOP/MyFirstCodeOOP/BaseCommissionEmployee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstCodeOOP
+{
+    public class BaseCommissionEmployee : CommissionEmployee
+    {
+        #region Properties
+        public decimal Base { get; set; }
+        #endregion
+
+        #region Methods
+        public override decimal GetValueTopay()
+        {
+            return base.GetValueTopay() + Base;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}" +
+                $"\n\tSalary Base................{Base:C2}" +
+                $"\n\tTotal to pay...............{GetValueTopay():C2}";
+        }
+        #endregion
+    }
+}
---
+++ MyFirstCodeOOP/MyFirstCodeOOP/ContractEmployee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstCodeOOP
+{
+    public class ContractEmployee: Employee
+    {
+        #region Properties
+        public float Hours { get; set; }
+        public decimal HourValue { get; set; }
+        #endregion
+
+
+        public override decimal GetValueTopay()
+        {
+            return ((decimal)Hours * HourValue);
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}" +
+                $"\n\tNumber of hours....{Hours}" +
+                $"\n\tValue per hour.....{HourValue:C2}" +
+                $"\n\tValue to pay.......{GetValueTopay():C2}";
+        }
+    }
+}
